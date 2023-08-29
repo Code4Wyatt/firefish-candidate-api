064be0ed0f8e8d19b21a333048e33043738f5472
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2023-08-29 17:52:12 +0100

    added CandidateSkill model class

--- firefish-candidate-api/Models/CandidateSkill.cs
+++ firefish-candidate-api/Models/CandidateSkill.cs
@@ -1,6 +1,14 @@
-﻿namespace firefish_candidate_api.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace firefish_candidate_api.Models
+
 {
     public class CandidateSkill
     {
+        public int SkillId { get; set; }
+        public int CandidateID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set;}
     }
 }
